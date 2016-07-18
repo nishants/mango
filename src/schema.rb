7bdd6faae84f79ab17ffb397d544fb80ff788f0e
nishant <nishant.singh87@gmail.com>
2016-07-19 05:18:11 +0530

    add schema declaration.

--- src/schema.rb
+++ src/schema.rb
@@ -1,2 +1,16 @@
 class Schema
+  def collections
+    Class.new {
+      def add config
+        puts "adding collection : #{config.to_s}"
+      end
+    }.new
+  end
+
+
+  def self.create(config)
+    puts "createing schema : #{config.to_s}"
+     Schema.new
+  end
+
 end
