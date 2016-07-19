7f96808f9e95c9bcc240fade433679f43926b9ea
nishant <nishant.singh87@gmail.com>
2016-07-19 09:02:50 +0530

    update collection.

--- src/schema.rb
+++ src/schema.rb
@@ -21,6 +21,21 @@ class Schema
   end
 
   def update args
+    @definition["collections"].each {|collection|
+      if(collection["name"] == args[:collection])
+
+        target_file = "#{args[:path]}/1011#{collection['path']}"
+        contents = JSON.parse(File.read(target_file))
+
+        contents["data"][args[:rename_to]] = contents["data"]["message"]
+        contents["data"].delete("message")
+        File.open(target_file, 'w'){|file|
+          file.write(contents.to_json() )
+        }
+      end
+    }
+
+    args[:path]
 
   end
 end
