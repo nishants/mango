09fde0e4580f142fc55afd7670434f9a61100413
nishant <nishant.singh87@gmail.com>
2016-07-20 18:47:50 +0530

    updating git ignore

---
+++ src/mango/profile.rb
@@ -0,0 +1,52 @@
+require 'pathname'
+require 'json'
+require_relative 'document'
+
+class Profile
+
+  def initialize path
+    @path = path
+    @schema = JSON.parse(File.read("#{@path}/schema.json"))
+  end
+
+  def all
+    profiles =  []
+    each_profile(){|profile, root, dir|
+      profile["dir"] = absolute_path dir;
+      profiles.push(profile);
+    }
+    profiles
+  end
+
+  def find name
+    each_profile(){|profile_json, profile_root|
+      if(profile_json["name"] == name)
+        result = {}
+        result["name"]        = profile_json["name"]
+        result["description"] = profile_json["description"]
+        @schema["files"].each{|file|
+          result[file["name"]] = Document.new(profile_root, file['path'])
+        }
+        return result
+      end
+    }
+    nil
+  end
+
+  def each_profile
+    Dir["#{@path}/*/"].each{ |profile|
+      profile_json = "#{profile}profile.json"
+      profile_root = "#{profile_json.sub("profile.json", "").strip()}"
+      if Pathname.new(profile_json).exist?
+        yield(JSON.parse(File.read(profile_json)), profile_root, profile)
+      end
+    }
+  end
+
+  def absolute_path path
+    return Pathname.new(path).realpath.to_s
+  end
+
+end
+
+
