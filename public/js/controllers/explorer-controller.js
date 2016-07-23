a90a11cd32ebf35fbfb734a5fa52de3dfe4f251d
nishant <nishant.singh87@gmail.com>
2016-07-23 12:48:12 +0530

    expand all folders on loading editor.

--- public/js/controllers/explorer-controller.js
+++ public/js/controllers/explorer-controller.js
@@ -3,14 +3,6 @@ app.controller("explorerController", ["$scope", "profilesService", "fileService"
         profiles: [],
         toggle : function (profile) {
             profile.__expand = !profile.__expand;
-        },
-        showFile : function (profile, file) {
-            $state.go("profile.edit", {name: profile.name, file: file.name})
-        },
-        expandOneProfile: function (name) {
-            explorer.profiles.forEach(function (profile) {
-                profile.__expand = (profile.name == name);
-            });
         }
     };
     var reset = function () {
@@ -19,11 +11,11 @@ app.controller("explorerController", ["$scope", "profilesService", "fileService"
             return fileService.updateSchema().then(function(files){
                 explorer.profiles.forEach(function (profile) {
                     profile.files = files;
+                    profile.__expand = true;
                 });
             })
         });        
     }
     reset();
-    explorer.expandOneProfile($state.params.name);
     $scope.explorer = explorer
 }]);
