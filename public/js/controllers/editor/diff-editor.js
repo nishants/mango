e0002471199db073c06d374c14802ce0793b483c
nishant <nishant.singh87@gmail.com>
2016-07-23 20:18:27 +0530

    diff editor as readonly.

--- public/js/controllers/editor/diff-editor.js
+++ public/js/controllers/editor/diff-editor.js
@@ -2,18 +2,32 @@ app.directive("diffEditor", [function () {
 
 return {
     restrict : "C",
-    scope : true, 
+    scope : true,
     transclude : false,
-    link :function (scope, element) {
-        var aceDiffer = new AceDiff({
-            mode: "ace/mode/json",
-            left: {
-                content: JSON.stringify(scope.editor.savedContent.base(), null, "\t"),
-            },
-            right: {
-                content: JSON.stringify(scope.editor.content, null, "\t"),
-            }
-        });
+    link :function ($scope) {
+
+        var aceDiff = new AceDiff({
+                mode: "ace/mode/json",
+                maxDiffs: 100, // No of max diff's to show.
+                left: {
+                    editable: false,
+                    content: JSON.stringify($scope.editor.savedContent.base(), null, "\t"),
+                },
+                right: {
+                    editable: false,
+                    content: JSON.stringify($scope.editor.content, null, "\t"),
+                }
+            }),
+            aceEditor = aceDiff.editors.right.ace;
+            aceEditor.on("change", function () {
+                try{
+                    $scope.$parent.editor.content = $scope.diffEditor.editors.right.ace.getValue();
+                }catch(e){
+                    console.log(e)
+                }
+            })
+
+        $scope.diffEditor = aceDiff;
     }
 };
 }]);
