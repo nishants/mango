ffb23491da0fc3ab0e6293ec801bf77c830044f5
nishant <nishant.singh87@gmail.com>
2016-07-24 12:18:41 +0530

    adding a data loader.

--- public/js/controllers/ui-controller.js
+++ public/js/controllers/ui-controller.js
@@ -1,9 +1,17 @@
 app.controller("uiController", ["$rootScope", "$scope", "profilesService", "searchService", "fileService", "$stateParams", function($rootScope, $scope, profilesService, searchService, fileService, $stateParams){
 
-    $scope.ui = {
-        state : $stateParams, 
-        search : searchService, 
-        profiles: profilesService,
-        schema: fileService,
-    };
+    var ui = {
+                state : $stateParams,
+                search : searchService,
+                profiles: profilesService,
+                schema: fileService,
+                loader: {show : true}
+        },
+        showLoader = function () {ui.loader.show = true;},
+        hideLoader = function () {ui.loader.show = false;};
+    
+    $rootScope.$on('$stateChangeStart', showLoader);
+    $rootScope.$on('$stateChangeSuccess',hideLoader);
+
+    $scope.ui = ui;
 }]);
