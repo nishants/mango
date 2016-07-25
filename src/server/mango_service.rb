cb45e0d3f464205692a0cd5d9b5eedce9b098f56
nishant <nishant.singh87@gmail.com>
2016-07-25 23:17:42 +0530

    add new project.

--- src/server/mango_service.rb
+++ src/server/mango_service.rb
@@ -9,7 +9,11 @@ module Mango
       JSON.parse(File.read(@config_file_path))["projects"]
     end
     def add_project name, path
-      JSON.parse(File.read(@config_file_path))["projects"]
+      config = JSON.parse(File.read(@config_file_path));
+      config["projects"].push({"name" => name, "path" => path})
+      File.open(@config_file_path, 'w'){|file|
+        file.write(config.to_json() )
+      }
     end
   end
 end
--- test/server/mango_service_spec.rb
+++ test/server/mango_service_spec.rb
@@ -6,9 +6,13 @@ RSpec.describe Mango::MangoService do
 
   describe "MangoService" do
 
-    before(:all) do
+    def absolute_path path
+      return Pathname.new(path).realpath.to_s
+    end
+
+    before(:each) do
       @config_file = "test/data/empty-config-file.json"
-      @empty_project_path = "test/data/new-project";
+      @empty_project_path = absolute_path "test/data/new-project";
       @service = Mango::MangoService.new(@config_file)
     end
 
@@ -17,9 +21,11 @@ RSpec.describe Mango::MangoService do
     end
 
     it "should add new project" do
-      expect(@service.projects).to eq([])
-      @service.add_project("project-name", @empty_project_path)
-      expect(@service.projects).to eq([])
+      project_name = "project-name"
+      project_path = @empty_project_path
+
+      @service.add_project(project_name, @empty_project_path)
+      expect(@service.projects).to eq([{"name" => project_name, "path" => project_path}])
     end
 
   end
