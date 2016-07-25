32938620a9bdb5df19f65911c0dad37d94e8bef3
nishant <nishant.singh87@gmail.com>
2016-07-26 00:12:59 +0530

    initialize new project.

--- src/server/mango_service.rb
+++ src/server/mango_service.rb
@@ -1,4 +1,5 @@
 require "json"
+require 'pathname'
 
 module Mango
   class MangoService
@@ -7,12 +8,32 @@ module Mango
       @config_file_path = config_file_path
     end
 
+    def if_exists file
+      Pathname.new(file).exist?
+    end
+
     def read_json file
       JSON.parse(File.read(file))
     end
 
+    def save_json file , content
+      File.open(file, 'w'){|file|
+        file.write(content.to_json)
+      }
+    end
+
     def projects
       read_json(@config_file_path)["projects"]
+    end
+
+    def create_project path
+      save_json "#{path}/mango.json", {"contracts" => [
+          {"name"        =>  "home",
+           "description"  => "response for users-service/<user-id>/home",
+           "path"         => "/home.json"}, {
+              "name"         => "companies",
+              "description"  => "response for quote-service/user/<user-id>/company",
+              "path"         => "/companies/all.json"}]}
     end
 
     def add_project name, path
@@ -21,6 +42,10 @@ module Mango
       File.open(@config_file_path, 'w'){|file|
         file.write(config.to_json() )
       }
+      project_settings = "#{path}/mango.json"
+      if(!if_exists project_settings)
+        create_project path
+      end
     end
 
     def contracts(project_name)
