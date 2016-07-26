09f84093e6c4d9666a68128ac1bfc99a5d1a0ecc
nishant <nishant.singh87@gmail.com>
2016-07-26 13:21:34 +0530

    refactoring.

--- test/server/contract_spec.rb
+++ test/server/contract_spec.rb
@@ -6,11 +6,7 @@ RSpec.describe Mango::Contract do
   describe "Contract Default Names" do
 
     it "should convert relative path to unique contract name" do
-      expected = {
-          "home.json" => "home",
-          "companies/all.json" => "companies-all"
-      }
-
+      expected = {"home.json" => "home", "companies/all.json" => "companies-all"}
       expected.keys.each{|path|
         expect(Mango::Contract.path_to_url path).to eq(expected[path])
       }
