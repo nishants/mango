3e073f364e7838919e4998a8c12844b5d58f9df9
nishant <nishant.singh87@gmail.com>
2016-08-07 18:43:15 +0530

    addin view project.

--- public/js/routes.js
+++ public/js/routes.js
@@ -21,6 +21,16 @@ app.config(function($stateProvider, $urlRouterProvider) {
                 }
             }
         })
+        .state('projects.view', {
+            url: "/:project",
+            templateUrl: "partials/list-profiles.html",
+            controller:  "listProfilesController",
+            resolve: {
+                profiles: function($stateParams, projectService) {
+                    return projectService.profilesOf($stateParams.project);
+                }
+            }
+        })
         .state('projects.profiles', {
             url: "/profiles/:project",
             templateUrl: "partials/list-profiles.html",
