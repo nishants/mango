7ebc2100063e93b880416a234a67862ace61c96f
nishant <nishant.singh87@gmail.com>
2016-08-08 15:50:05 +0530

    remove old api.

--- public/js/routes.js
+++ public/js/routes.js
@@ -1,16 +1,6 @@
 app.config(function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise("/");
+    $urlRouterProvider.otherwise("/projects");
     $stateProvider
-        .state('home', {
-            url: "/",
-            templateUrl: "partials/home.html",
-            resolve: {
-                loadProfile: function(searchService) {
-                    return searchService.profiles.search();
-                }
-            }
-
-        })
         .state('projects', {
             url: "/projects",
             templateUrl: "partials/projects.html",
@@ -49,55 +39,5 @@ app.config(function($stateProvider, $urlRouterProvider) {
                     return projectService.getFile($stateParams.project, $stateParams.profile, $stateParams.contract);
                 }
             }
-        })
-       // .state('powermode', {
-       //     url: "/power",
-       //     templateUrl: "partials/powermode.html",
-       //     resolve: {
-       //         loadData: function(searchService, profilesService, fileService) {
-       //             return searchService.profiles.search().then(function (profiles){
-       //                 return profilesService.findByName(profiles[0].name).then(function(){
-       //                     return fileService.updateSchema().then(function(files){
-       //                         return fileService.getFile(profiles[0].name, files[0].name);
-       //                     });
-       //                 });
-       //             });
-       //         }
-       //     }
-       //
-       // })
-       //.state('profile', {
-       //    url: "/profiles/:name",
-       //    templateUrl: "partials/profile.html",
-       //    controller: "viewProfileController",
-       //    resolve: {
-       //        loadProfile: function($stateParams, profilesService, searchService, fileService) {
-       //            return fileService.updateSchema().then(function () {
-       //                return searchService.files.search().then(function () {
-       //                    return profilesService.findByName($stateParams.name);
-       //                });
-       //            });
-       //        }
-       //    }
-       //})
-       //.state('profile.edit', {
-       //    url: "/:file/edit",
-       //    templateUrl: "partials/editor.html",
-       //    controller: "editorController",
-       //    resolve: {
-       //        fetchFile: function($stateParams, fileService) {
-       //            return fileService.getFile($stateParams.name, $stateParams.file);
-       //        }
-       //    }
-       //})
-       // .state('profile.edit.diff', {
-       //    url: "/diff",
-       //    templateUrl: "partials/editor/diff.html",
-       //    resolve: {
-       //        fetchFile: function($stateParams, fileService) {
-       //            return fileService.getFile($stateParams.name, $stateParams.file);
-       //        }
-       //    }
-       //})
-    ;
+        });
 });
