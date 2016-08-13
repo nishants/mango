3794b4d00ddfc923427b59983c0da7dae460543e
nishant <nishant.singh87@gmail.com>
2016-08-13 23:34:58 +0530

    cleaning up.

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,1 @@
 window.app = angular.module("mango", ["ui.router", "ng.jsoneditor"]);
-window.app.run(function (projectService) {
-    return projectService.all().then(function () {
-        if(projectService.list.length > 0){projectService.select(projectService.list[0].name);}
-    });
-});
